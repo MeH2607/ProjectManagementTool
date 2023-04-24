4e263f48c03eaee330f8ff94a228dd0648b06da3
paul7299 <paul7299@stud.kea.dk>
2023-04-24 12:46:35 +0200

    main classes created

---
+++ src/main/java/com/example/projectmanagementtool/Model/Subproject.java
@@ -0,0 +1,7 @@
+package com.example.projectmanagementtool.Model;
+
+public class Subproject{
+    String name; // name of project, subproject, task etc
+    double eta; //
+
+}
---
+++ src/main/java/com/example/projectmanagementtool/Model/Task.java
@@ -0,0 +1,7 @@
+package com.example.projectmanagementtool.Model;
+
+public class Task{
+    String name; // name of project, subproject, task etc
+    double eta; //
+
+}
