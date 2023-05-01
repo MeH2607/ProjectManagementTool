9d1ed25e2b0dfd7e7ed97d481e340a58c648b22d
asgertor <Asgersensen@gmail.com>
2023-05-01 20:12:40 +0200

    Basic overview of subprojects and updated models

--- src/main/java/com/example/projectmanagementtool/Controller/PmtController.java
+++ src/main/java/com/example/projectmanagementtool/Controller/PmtController.java
@@ -1,11 +1,13 @@
 package com.example.projectmanagementtool.Controller;
 import com.example.projectmanagementtool.Model.Project;
+import com.example.projectmanagementtool.Model.Subproject;
 import com.example.projectmanagementtool.Model.Task;
 import com.example.projectmanagementtool.Service.PMTService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,23 +21,26 @@ public class PmtController {
         this.pmtService = pmtService;
     }
 
-    @GetMapping("")
-    public String index(Model model, HttpSession session) {
+    @GetMapping("{subProjectID}")
+    public String index(@PathVariable int subProjectID, Model model, HttpSession session) {
         Task task = new Task();
         model.addAttribute("task", task);
-        List<Task> list = pmtService.getAllTasks();
+        List<Task> list = pmtService.getTasksFromSubproject(subProjectID);
         model.addAttribute("list", list);
 
         return "index";
     }
-    @GetMapping("subprojects")
-    public String subProjectOverview(Model model, HttpSession session) {
-        Task task = new Task();
-        Project project = new Project();
-        model.addAttribute("task", task);
-        model.addAttribute("project", project);
-        List<Task> list = pmtService.getAllTasks();
-        model.addAttribute("list", list);
+    @GetMapping("project/{projectID}")
+    public String subProjectOvervisew(@PathVariable int projectID,  Model model, HttpSession session) {
+
+        List<Task> tasks = pmtService.getAllTasks();
+        List<Subproject> subprojects = pmtService.getSubProjects(projectID);
+
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("subprojects", subprojects);
+
+        List<Task> list = pmtService.getTasksFromSubproject(projectID);
+
 
         return "subprojects";
     }
